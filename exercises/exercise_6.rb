54c84f43a4f8bfb6a237821431a40703bc78a718
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-09 00:06:44 +0000

    Fix hourly rate for employees based on exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,9 +10,9 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Alky", last_name: "Ohra", hourly_rate: 30)
-@store1.employees.create(first_name: "Hanna", last_name: "Gready", hourly_rate: 30)
-@store1.employees.create(first_name: "Kris", last_name: "Bold", hourly_rate: 30)
+@store1.employees.create(first_name: "Alky", last_name: "Ohra", hourly_rate: 40)
+@store1.employees.create(first_name: "Hanna", last_name: "Gready", hourly_rate: 40)
+@store1.employees.create(first_name: "Kris", last_name: "Bold", hourly_rate: 40)
 @store2.employees.create(first_name: "Helen", last_name: "Wtire", hourly_rate: 55)
 @store2.employees.create(first_name: "Colyn", last_name: "Dale", hourly_rate: 45)
 @store2.employees.create(first_name: "Derek", last_name: "Collens", hourly_rate: 65)
